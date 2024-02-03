f17048a9245fdc1f77c9043c7fd62734b7952789
David Vychanek <vychanek@vychanek.cz>
2024-02-03 22:35:14 +0100

    comitt3

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ const observer = new IntersectionObserver((entries)=> {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+
+const observerTwo = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('showTwo');
+        }
+        // else {
+        //     entry.target.classList.remove('show');
+        // }
+    });
+}); 
+
+
+const hiddenElementsTwo = document.querySelectorAll('.logo');
+hiddenElementsTwo.forEach((el) => observerTwo.observe(el));
